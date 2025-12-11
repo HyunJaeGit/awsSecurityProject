3a82b2d3d73559ff84c9817832d8d92c0f02fa9f
guswo1118 <guswo1118@naver.com>
2025-12-12 03:13:03 +0900

    Initial commit

---
+++ src/main/java/com/security/project/awssecurityproject/AwsSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.security.project.awssecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSecurityProjectApplication.class, args);
+    }
+
+}
